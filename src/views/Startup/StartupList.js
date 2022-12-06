fe35f2600c1b0f943024fa6772d587d02f79da5e
Isal <58117968+rfzsal@users.noreply.github.com>
2022-12-06 15:48:25 +0700

    Add StartupList view

---
+++ src/views/Startup/StartupList.js
@@ -0,0 +1,33 @@
+import propTypes from 'prop-types';
+import { View } from 'react-native';
+
+import DataList from '../../components/DataList';
+
+const StartupList = ({ startups }) => {
+  return (
+    <View>
+      {startups.map((mentor, index) => {
+        return (
+          <DataList
+            key={mentor.name + index}
+            name={mentor.name}
+            description={mentor.description}
+            location={mentor.address}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+StartupList.propTypes = {
+  startups: propTypes.arrayOf(
+    propTypes.shape({
+      name: propTypes.string,
+      description: propTypes.string,
+      location: propTypes.string,
+    })
+  ),
+};
+
+export default StartupList;
