bd6e41e503bec9508b29cda58a2cc65615aa23a8
Isal <58117968+rfzsal@users.noreply.github.com>
2022-12-08 15:31:49 +0700

    Add Matchmaking page

---
+++ src/pages/Matchmaking/index.js
@@ -0,0 +1,77 @@
+import {
+  setStatusBarBackgroundColor,
+  setStatusBarStyle,
+} from 'expo-status-bar';
+import { useEffect } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { useTheme, TextInput, Appbar } from 'react-native-paper';
+
+import MITRA from '../../_DATA/mitra.json';
+import Container from '../../layout/Container';
+import MitraList from '../../views/Matchmaking/MitraList';
+
+const Search = () => {
+  const { colors } = useTheme();
+  return (
+    <Container mt={8} mb={16}>
+      <TextInput
+        placeholder="Search activity..."
+        left={
+          <TextInput.Icon
+            name="magnify"
+            color={colors.disabled}
+            rippleColor={colors.background}
+            style={styles.searchIcon}
+          />
+        }
+        mode="outlined"
+        style={{ backgroundColor: colors.surface }}
+      />
+    </Container>
+  );
+};
+
+const Matchmaking = ({ navigation }) => {
+  const { colors } = useTheme();
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      setStatusBarBackgroundColor(colors.surface);
+      setStatusBarStyle('dark');
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  return (
+    <>
+      <Appbar.Header
+        style={{
+          backgroundColor: colors.surface,
+        }}
+      >
+        <Appbar.BackAction
+          onPress={() => {
+            navigation.goBack();
+          }}
+        />
+        <Appbar.Content title="Mitra" />
+      </Appbar.Header>
+
+      <ScrollView
+        style={{ backgroundColor: colors.surface }}
+        contentContainerStyle={styles.scrolViewContent}
+      >
+        <Search />
+        <MitraList mitras={MITRA} />
+      </ScrollView>
+    </>
+  );
+};
+
+export default Matchmaking;
+
+const styles = StyleSheet.create({
+  searchIcon: { top: 2 },
+  scrolViewContent: { paddingBottom: 16 },
+});
