45a98b3bbc50d9aa573ebb4c894347f5656d151f
Isal <58117968+rfzsal@users.noreply.github.com>
2022-12-09 15:31:28 +0700

    Update login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,17 +2,35 @@ import {
   setStatusBarBackgroundColor,
   setStatusBarStyle,
 } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  ToastAndroid,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import Spinner from 'react-native-loading-spinner-overlay/lib';
-import { Button, useTheme } from 'react-native-paper';
+import { TextInput, Button, useTheme, Colors } from 'react-native-paper';
 
 import SafeAreaView from '../../components/SafeAreaView';
 import { useAuth } from '../../hooks/useAuth';
+import Container from '../../layout/Container';
 
 const Login = ({ navigation }) => {
   const { colors } = useTheme();
   const auth = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    const res = await auth.login(email, password);
+    if (res?.error)
+      ToastAndroid.show('Invalid username or password', ToastAndroid.SHORT);
+  };
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
@@ -25,17 +43,68 @@ const Login = ({ navigation }) => {
 
   return (
     <SafeAreaView>
-      <View style={styles.textContainer}>
-        <Text>Login</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          mode="contained"
-          onPress={async () => auth.login('user@aimee.id', 'user-123')}
-        >
-          Login
-        </Button>
-      </View>
+      <ScrollView>
+        <Container mt={64}>
+          <View style={styles.imageContainer}>
+            <Image
+              style={styles.image}
+              source={require('../../../assets/aimee.png')}
+            />
+          </View>
+        </Container>
+
+        <Container mt={8}>
+          <TextInput
+            label="Email"
+            value={email}
+            onChangeText={setEmail}
+            mode="outlined"
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+            mode="outlined"
+            secureTextEntry
+            style={styles.passwordInput}
+          />
+
+          <Button
+            onPress={handleLogin}
+            mode="contained"
+            style={styles.loginButton}
+          >
+            Login
+          </Button>
+        </Container>
+
+        <Container mt={24}>
+          <Text
+            style={
+              (styles.textRegister,
+              {
+                color: Colors.grey600,
+              })
+            }
+          >
+            Don't have an account?
+          </Text>
+          <View style={styles.registerButton}>
+            <TouchableOpacity onPress={() => {}} activeOpacity={0.6}>
+              <Text
+                style={[
+                  styles.registerButtonText,
+                  {
+                    color: colors.primary,
+                  },
+                ]}
+              >
+                Register Now!
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Container>
+      </ScrollView>
 
       {auth.loading && <Spinner visible />}
     </SafeAreaView>
@@ -45,18 +114,15 @@ const Login = ({ navigation }) => {
 export default Login;
 
 const styles = StyleSheet.create({
-  superContainer: {
-    flex: 1,
-  },
-  textContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
+  imageContainer: { flex: 1, alignItems: 'center' },
+  image: { height: 100, resizeMode: 'contain', right: 1 },
+  passwordInput: { marginTop: 8 },
+  loginButton: { marginTop: 16 },
+  textRegister: { textAlign: 'center' },
+  registerButton: { alignSelf: 'center' },
+  registerButtonText: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    textAlign: 'center',
   },
 });
